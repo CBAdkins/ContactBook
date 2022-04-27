d4589059445df3629292f63893dbcd39fec520db
Christopher B. Adkins <adkins1984@gmail.com>
2022-04-28 00:40:14 +0200

    Update app-routing.module.ts
    Wrong app-routing.module.ts was in the initial commit

--- contactbook-client-web/src/app/app-routing.module.ts
+++ contactbook-client-web/src/app/app-routing.module.ts
@@ -1,26 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyContactsComponent } from './my-contacts/my-contacts.component';
-import { ContactAddComponent } from './contact-add/contact-add.component';
-import { ContactEditComponent } from './contact-edit/contact-edit.component';
-import { ContactGetComponent } from './contact-get/contact-get.component';
 
 const routes: Routes = [
 	{ 
 		path:'', 
 		component: MyContactsComponent
-	},
-	{
-		path: 'contact/create',
-		component: ContactAddComponent
-	},
-	{
-		path: 'edit/:id',
-		component: ContactEditComponent
-	},
-	{
-		path: 'contact',
-		component: ContactGetComponent
 	}
 ];
 
